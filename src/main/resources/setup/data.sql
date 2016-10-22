edd49c2d37ef5ff51f33c42342daf752298b99d3
Anton Syzko <antonio.shizko@gmail.com>
2016-10-22 21:31:07 +0100

    Deploy

--- src/main/resources/setup/data.sql
+++ src/main/resources/setup/data.sql
@@ -1,2 +1,24 @@
 INSERT INTO visitor (ipAddress,countryCode,countryName,region,regionName,city,postalCode,latitude,longtitude,visitedAt) VALUES
   ('127.0.0.0.1','US','USA','Texas','Texas','Texas','22','123.123','3453.3453','2016-09-29 00:00:00');
+
+
+-- CREATE TABLE `users` (
+--   `username` varchar(50) NOT NULL,
+--   `password` varchar(255) NOT NULL,
+--   `enabled` tinyint(1) NOT NULL,
+--   PRIMARY KEY (`username`)
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
+INSERT INTO users (username,password,enabled) VALUES
+('test','$2a$10$VE93gbba.AsZxOv.uTY4kuLoYGaYt5fKABjGXdDROvrVSY1PSMbVy',1);
+
+-- CREATE TABLE authorities (
+--   username varchar(50) NOT NULL,
+--   authority varchar(50) NOT NULL,
+--   CONSTRAINT authorities_idx_1 UNIQUE  (username,authority),
+--   CONSTRAINT authorities_ibfk_1 FOREIGN KEY (username) REFERENCES users (username)
+-- )
+
+INSERT INTO authorities (
+username,authority
+) VALUES ('test','ADMIN');
--- src/main/resources/setup/schema.sql
+++ src/main/resources/setup/schema.sql
@@ -1,36 +1,37 @@
-CREATE TABLE `visitor` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `ipAddress` varchar(255) NOT NULL,
-    `countryCode` varchar(255) NOT NULL,
-  `countryName` varchar(255) NOT NULL,
-  `region` varchar(255) NOT NULL,
-  `regionName` varchar(255) NOT NULL,
-  `city` varchar(255) NOT NULL,
-  `postalCode` varchar(255) NOT NULL,
-  `latitude` varchar(255) NOT NULL,
-    `longtitude` varchar(255) NOT NULL,
-  `visitedAt` datetime DEFAULT NULL,
+CREATE SEQUENCE visitor_seq;
 
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8
+CREATE TABLE visitor (
+  id bigint NOT NULL DEFAULT NEXTVAL ('visitor_seq'),
+  ip_address varchar(255) NOT NULL,
+    country_code varchar(255) NOT NULL,
+  country_name varchar(255) NOT NULL,
+  region varchar(255) NOT NULL,
+  region_name varchar(255) NOT NULL,
+  city varchar(255) NOT NULL,
+  postal_code varchar(255) NOT NULL,
+  latitude varchar(255) NOT NULL,
+    longtitude varchar(255) NOT NULL,
+  visited_at timestamp(0) DEFAULT NULL,
 
+  PRIMARY KEY (id)
+)
+
+alter sequence visitor_seq restart with 0;
+
+
+
+CREATE TABLE authorities (
+  username varchar(50) NOT NULL,
+  authority varchar(50) NOT NULL,
+  CONSTRAINT authorities_idx_1 UNIQUE  (username,authority),
+  CONSTRAINT authorities_ibfk_1 FOREIGN KEY (username) REFERENCES users (username)
+)
 
 
 
 CREATE TABLE users (
-  username VARCHAR(100) NOT NULL PRIMARY KEY,
-  password VARCHAR(100) NOT NULL,
-  enabled BOOLEAN NOT NULL
-);
-
-CREATE TABLE authorities (
-  username VARCHAR(100) NOT NULL,
-  authority VARCHAR(100) NOT NULL,
-
-  CONSTRAINT fk_authorities_users
-  FOREIGN KEY(username) REFERENCES users(username)
-);
-CREATE UNIQUE INDEX ix_auth_username ON authorities(username,authority);
-
-
-
+  username varchar(50) NOT NULL,
+  password varchar(255) NOT NULL,
+  enabled smallint NOT NULL,
+  PRIMARY KEY (username)
+)
